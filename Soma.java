9a18bd12fedc1bdcf374c2ced2fb3f34223196cc
ERICK TAVARES NUNES <erick.nunes@a.ucb.br>
2024-09-05 10:34:44 -0300

    Atividade Soma

---
+++ Soma.java
@@ -0,0 +1,12 @@
+package AtividadeAva;
+
+public class Soma {
+    public int somar (int a, int b){
+        return a + b;
+    }
+
+    public double somar(double a, double b){
+        return a + b;
+    }
+
+}
